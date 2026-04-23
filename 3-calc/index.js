ddf2ca6b75b60a3d2f77b63db0052f3e7f8856d5
Anton <anton.t@railsmuffin.com>
2026-04-23 21:38:49 +0300

    HW - fix comment

--- 3-calc/index.js
+++ 3-calc/index.js
@@ -2,6 +2,7 @@ import { add } from "./operations/add.js";
 import { minus } from "./operations/minus.js";
 import { multiply } from "./operations/multiply.js";
 import { divide } from "./operations/divide.js";
+import EventEmitter from 'node:events'
 
 const actions = {add, minus, multiply, divide}
 
@@ -9,9 +10,19 @@ const num1 = Number(process.argv[2])
 const num2 = Number(process.argv[3])
 const operation = process.argv[4]
 
-if (actions[operation]){
-	const result = actions[operation](num1,num2);
-	console.log(result)
-} else {
-	console.log("unknown actions")
-}
+const myEmit = new EventEmitter();
+
+myEmit.on('result', (value) => {
+	console.log('Res = ' + value)
+} )
+
+myEmit.on(operation, (a,b) => {
+	if(actions[operation]){
+		const res = actions[operation](a,b);
+		myEmit.emit('result',res)
+	} else {
+		console.log('uknown action')
+	}
+})
+
+myEmit.emit(operation, num1, num2)
